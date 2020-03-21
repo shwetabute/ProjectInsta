181a424ba35a053caef4c78f73e3508df5d5f717
PoojaBayas <pooja.bayas32@gmail.com>
2020-03-20 21:14:37 -0700

    changed the file name to users

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
 //DB config
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
 
 //connect to mongodb
 mongoose
-.connect(db)
-.then(() => console.log('MongoDb connected!'))
-.catch(err => console.log(err));
-
+  .connect(db)
+  .then(() => console.log("MongoDb connected!"))
+  .catch(err => console.log(err));
 
 //Let's write our first route
-app.get('/', (req,res) => res.send('Hello World!'));
+app.get("/", (req, res) => res.send("Hello World!"));
 
-app.use('/api/users', users);
-
+app.use("/api/users", users);
+app.use("/api/profile", profile);
 
 const port = 8060;
 app.listen(port, () => console.log(`Server running on port ${port}`));
-
