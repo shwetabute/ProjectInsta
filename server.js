3cd3ade1fece3f152063e672418fa63109cbf70f
PoojaBayas <pooja.bayas32@gmail.com>
2020-03-20 21:44:45 -0700

    checked the register route

--- server.js
+++ server.js
@@ -1,22 +1,35 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const app = express();
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
-//DB config
+// const posts = require("./routes/api/posts");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const app = express();
+
+//body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//congif DB
 const db = require("./config/keys").mongoURI;
 
-//connect to mongodb
+//connect to db
 mongoose
   .connect(db)
-  .then(() => console.log("MongoDb connected!"))
+  .then(() => console.log("Mongo Db connected"))
   .catch(err => console.log(err));
 
-//Let's write our first route
-app.get("/", (req, res) => res.send("Hello World!"));
+//Passport middleware
+app.use(passport.initialize());
+//Passport config
+require("./config/passport")(passport);
 
+//lets write our first route
+app.get("/", (req, res) => res.send("Hello "));
 app.use("/api/users", users);
 app.use("/api/profile", profile);
+// app.use("/api/posts", posts);
 
 const port = 8060;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`server running on the port ${port}`));
