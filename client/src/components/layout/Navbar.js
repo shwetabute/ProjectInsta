017a6d3117eb3d6bc8e78801c6d28bde3f0c3c11
PoojaBayas <pooja.bayas32@gmail.com>
2020-04-09 17:39:50 -0700

    started with react and created a login,registration page

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+ class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        Hey this is navbar
+      </div>
+    )
+  }
+}
+export default Navbar;
