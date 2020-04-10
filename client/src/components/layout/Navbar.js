af8825b6160b7bde87db5965f32972fa39a24463
PoojaBayas <pooja.bayas32@gmail.com>
2020-04-09 21:09:32 -0700

    merged the navbar , footer , and login page

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,12 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
- class Navbar extends Component {
+class Navbar extends Component {
   render() {
     return (
-      <div>
-        Hey this is navbar
-      </div>
-    )
+      <nav>
+        <div className="nav-wrapper">
+          <Link to="/" className="brand-logo LinkNav">
+            Instagram
+          </Link>
+
+          <ul className="right hide-on-med-and-down">
+            <li>
+              <Link to="/" className="#8FD8D8 LinkNav">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/register" className="LinkNav">
+                Signup
+              </Link>
+            </li>
+            <li>
+              <Link to="/login" className="LinkNav">
+                Login
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
 export default Navbar;
