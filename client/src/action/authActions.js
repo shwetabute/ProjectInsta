bc1f6f89642da94d6611c97dc97248b8503f02bb
PoojaBayas <pooja.bayas32@gmail.com>
2020-04-14 18:56:08 -0700

    created a reducer for register, able to store user's data to store , and fetch store data back on compo

---
+++ client/src/action/authActions.js
@@ -0,0 +1,8 @@
+import { SET_CURRENT_USER } from "./types";
+
+export const registerUser = (userData) => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: userData,
+  };
+};
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import axios from "axios";
 import classnames from "classnames";
 import "../layout/style_login.css";
+import { registerUser } from "../../action/authActions";
+import { connect } from "react-redux";
 
 class Register extends Component {
   constructor() {
@@ -11,7 +13,7 @@ class Register extends Component {
       email: "",
       password: "",
       password2: "",
-      errors: {}
+      errors: {},
     };
 
     this.onChange = this.onChange.bind(this);
@@ -28,96 +30,103 @@ class Register extends Component {
       name: this.state.name,
       email: this.state.email,
       password: this.state.password,
-      password2: this.state.password2
+      password2: this.state.password2,
     };
 
-    axios
-      .post("/api/users/register", newUser)
-      .then(res => console.log(res.data))
-      .catch(err => this.setState({ errors: err.response.data }));
+    // axios
+    //   .post("/api/users/register", newUser)
+    //   .then((res) => console.log(res.data))
+    //   .catch((err) => this.setState({ errors: err.response.data }));
+    this.props.registerUser(newUser);
   }
 
   render() {
     const { errors } = this.state;
+    const { user } = this.props.auth;
 
     return (
-        <div className="style_login">
-          {/* <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"></link>   */}
-            <div className="row no-gutters">
-              <div className="col">
-                <div className="leftside">
-                  <div className="body"></div>
-                </div>
-              </div>
-            <div className="col">
-              <div className="rightside"></div>        
-                <form onSubmit={this.onSubmit}>
-                <div className="login">
-                <div>
-                  <div className="wc_text">Sign Up to ProjectInsta</div><br/>
-                  </div>
-                    <input
-                      type="text"
-                      className={classnames("sign_up_text", {
-                        "is-invalid": errors.name
-                      })}
-                      placeholder="Name"
-                      name="name"
-                      value={this.state.name}
-                      onChange={this.onChange}
-                    />
-                    {errors.name && (
-                      <div className="invalid-feedback">{errors.name}</div>
-                    )}
-                    <input
-                      type="email"
-                      className={classnames("sign_up_email", {
-                        "is-invalid": errors.email
-                      })}
-                      placeholder="Email Address"
-                      name="email"
-                      value={this.state.email}
-                      onChange={this.onChange}
-                    />
-                    {errors.email && (
-                      <div className="invalid-feedback">{errors.email}</div>
-                    )}
-                    <input
-                      type="password"
-                      className={classnames("sign_up_email", {
-                        "is-invalid": errors.password
-                      })}
-                      placeholder="Password"
-                      name="password"
-                      value={this.state.password}
-                      onChange={this.onChange}
-                    />
-                    {errors.password && (
-                      <div className="invalid-feedback">{errors.password}</div>
-                    )}
-                    <input
-                      type="password"
-                      className={classnames("sign_up_email", {
-                        "is-invalid": errors.password2
-                      })}
-                      placeholder="Confirm Password"
-                      name="password2"
-                      value={this.state.password2}
-                      onChange={this.onChange}
-                    />
-                    {errors.password2 && (
-                      <div className="invalid-feedback">{errors.password2}</div>
-                    )}
-
-                    <input type="submit" className="btn_submit" value="Sign Up"/>
-                  </div>
-                </form>
-              </div>
+      <div className="style_login">
+        {/* <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"></link>   */}
+        <div className="row no-gutters">
+          <div className="col">
+            <div className="leftside">
+              <div className="body"></div>
             </div>
           </div>
-      );
-    }
+          <div className="col">
+            <div className="rightside"></div>
+            <form onSubmit={this.onSubmit}>
+              <div className="login">
+                <div>
+                  {user?user.name:null}
+                  <div className="wc_text">Sign Up to ProjectInsta</div>
+                  <br />
+                </div>
+                <input
+                  type="text"
+                  className={classnames("sign_up_text", {
+                    "is-invalid": errors.name,
+                  })}
+                  placeholder="Name"
+                  name="name"
+                  value={this.state.name}
+                  onChange={this.onChange}
+                />
+                {errors.name && (
+                  <div className="invalid-feedback">{errors.name}</div>
+                )}
+                <input
+                  type="email"
+                  className={classnames("sign_up_email", {
+                    "is-invalid": errors.email,
+                  })}
+                  placeholder="Email Address"
+                  name="email"
+                  value={this.state.email}
+                  onChange={this.onChange}
+                />
+                {errors.email && (
+                  <div className="invalid-feedback">{errors.email}</div>
+                )}
+                <input
+                  type="password"
+                  className={classnames("sign_up_email", {
+                    "is-invalid": errors.password,
+                  })}
+                  placeholder="Password"
+                  name="password"
+                  value={this.state.password}
+                  onChange={this.onChange}
+                />
+                {errors.password && (
+                  <div className="invalid-feedback">{errors.password}</div>
+                )}
+                <input
+                  type="password"
+                  className={classnames("sign_up_email", {
+                    "is-invalid": errors.password2,
+                  })}
+                  placeholder="Confirm Password"
+                  name="password2"
+                  value={this.state.password2}
+                  onChange={this.onChange}
+                />
+                {errors.password2 && (
+                  <div className="invalid-feedback">{errors.password2}</div>
+                )}
+
+                <input type="submit" className="btn_submit" value="Sign Up" />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
   }
-  
-  export default Register;
-  
+}
+
+const mapStateToProps = (state) => ({
+  auth: state.auth
+})
+
+export default connect(mapStateToProps, { registerUser })(Register);
