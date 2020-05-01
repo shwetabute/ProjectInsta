f3de1db130f7c7f16c5d33ae30bd87b7cb423bd2
noorhilmi <47267410+noorhilmi@users.noreply.github.com>
2020-05-01 10:12:17 -0700

    .

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -102,8 +102,9 @@ class Navbar extends Component {
             className="navbar-toggler"
             type="button"
             data-toggle="collapse"
-            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
-             >
+            data-target="#mobile-nav"
+            data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon"></span>
           </button>
 
