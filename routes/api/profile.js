1f5807b7257e42307f794b4455420b58832c47cf
PoojaBayas <pooja.bayas32@gmail.com>
2020-05-02 23:09:46 -0700

    just minor change

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -57,8 +57,7 @@ router.post(
           }
           //Save profile
           new Profile(profileFields).save().then(profile => res.json(profile));
-          // new Profile(profileFields).save()
-          // .then(profile => res.json(profile));
+         
         });
       }
     });
